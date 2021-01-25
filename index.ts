561e25a2f4fafefc5395d1b63a86770680fef0ed
Molokoda <prototipich@gmail.com>
2021-01-25 23:51:47 +0300

    create express server

---
+++ index.ts
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+const userRouter = require('./routes/user.routes.ts');
+app.use('/public', express.static('public'));
+
+app.use(bodyParser.json());
+app.use( bodyParser.urlencoded( {extended: true} ) );
+app.use('/users', userRouter);
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+})
